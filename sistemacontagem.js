6ff0015b46e908c20eda5395a5b9dacd10f2c768
Renan Pereira <renan.rpo@hotmail.com>
2021-10-28 23:09:46 -0300

    Informar se o numero é 0, par ou impar. Utilizar concatenação e interpolação

--- sistemacontagem.js
+++ sistemacontagem.js
@@ -1,9 +1,15 @@
 let numeroAlunos = 5;
 
 for(let contador = 0; contador < numeroAlunos; contador++) {
-  console.log(contador)
+  //console.log(contador)
 
   if(contador == 0){
-    console.log("Zero");
-  }
-  };
+    console.log(contador + ": ZERO");  // concatenação
+  } 
+    else if (contador % 2 == 1){
+      console.log(`${contador}: ÍMPAR`) // interpolação - templates strings
+  } 
+    else {
+      console.log(`${contador}: PAR`)
+  }  
+};
