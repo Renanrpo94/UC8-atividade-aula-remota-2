0e893856369f93deed3a42a997c527183dff9f10
Renan Pereira <renan.rpo@hotmail.com>
2021-10-29 23:12:16 -0300

    Trabalho pronto

--- sistemacontagem.js
+++ sistemacontagem.js
@@ -3,10 +3,10 @@ let numeroAlunos = 6;
 let listaAlunos = ["Chimbinha","Fulano","Beltrano","Joazinho","Mariazinha","Renan"];
 
 for(let contador = 0; contador < listaAlunos.length; contador++) {
-  //console.log(contador)
+  
 
   if(contador == 0){
-    console.log(contador + ": ZERO: " + listaAlunos[contador]);  // concatenação
+    console.log(contador + ": ZERO: " + listaAlunos[contador]);
   } 
     else if (contador % 2 == 1){
       console.log(`${contador}: ÍMPAR: ${listaAlunos[contador]}`)
